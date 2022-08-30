543050177c29a791a5d9fee7e41b305ab95b6139
kor1du <65334980+kor1du@users.noreply.github.com>
2022-08-30 18:59:06 +0900

    2022-08-30

---
+++ 6. 문자열/3. 10809.js
@@ -0,0 +1,53 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+readline
+  .on("line", (line) => {
+    // 공백을 기준으로
+    input.push(line);
+  })
+  .on("close", function () {
+    //이 안에 솔루션 코드 작성
+    solution(input);
+    process.exit();
+  });
+
+const solution = (input) => {
+  const word = input[0];
+  const alphabetArray = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  alphabetArray.forEach((val, index) => {
+    console.log(word.indexOf(val));
+  });
+};
